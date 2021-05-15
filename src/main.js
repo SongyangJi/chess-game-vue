e9799fb4a4e7dc4eff6fac8480bdcf87b3f6e2a9
SongyangJi <1539242799@qq.com>
2021-05-16 00:40:17 +0800

    添加 element依赖

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+
+// 导入 ElementUI组件
+import ElementUI from 'element-ui'
+// 样式需要单独导入
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 
 new Vue({
   render: h => h(App),
