055485ea1381af5ef8c6f087ee2ed6332c18df48
SongyangJi <1539242799@qq.com>
2021-05-16 16:34:12 +0800

    单机环境下"画出"棋子

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -3,7 +3,7 @@
     <el-header style="height: 20px; display: block; text-align: center">
     </el-header>
     <el-main>
-      <canvas :id="roomId" @click="onClick()">Your browser doesn't support canvas</canvas>
+      <canvas id="board" @click="onClick">Your browser doesn't support canvas</canvas>
     </el-main>
     <el-footer style="height: 20px">
     </el-footer>
@@ -13,33 +13,32 @@
 <script>
 
 //canvas边长
+
+import COLOR from '@/constants/color'
+
 const side = 495
 // 棋盘格子边长
-// const d = 33
+const d = 33
 //棋盘格子距离棋盘边缘的距离
-// const m = d / 2
+const m = d / 2
 //棋子半径
-// const r = 15
+const r = 15
 
 export default {
   name: "ChessBoard",
-  props: ['roomId'],
   data() {
-    // let canvas = document.getElementById(this.roomId);
-    // let context = canvas.getContext('2d');
     return {
-      // canvas,
+      // canvas 画板
       context: {},
       steps: [],
-      myColor: -1,
-      // title: this.$t('lang.chessboard.message.clickReady'),
+      myColor: 0,
       started: false,
       waitResponse: false
     }
   },
   methods: {
     initCanvas() {
-      let canvas = document.getElementById(this.roomId);
+      let canvas = document.getElementById("board");
       canvas.width = side
       canvas.height = side
       this.context = canvas.getContext('2d');
@@ -47,10 +46,72 @@ export default {
     onClick(e) {
       let x = e.offsetX
       let y = e.offsetY
+      // console.log("(x,y) ",x,y)
       let i = Math.floor(x / d)
       let j = Math.floor(y / d)
-      console.log("(i,j)  ", i, j);
-    }
+      console.log("(i,j)  ", i, j)
+
+      // 如果没有下过此格点
+      if (!this.hasStep(i, j)) {
+        this.chess(i, j)
+      }
+
+    },
+    chess(i, j) {
+      this.steps.push({i, j})
+      this.drawChess(i, j, this.myColor)
+      this.drawLabel(i, j)
+    },
+    /**
+     *
+     * @param i 棋盘坐标 -- i
+     * @param j 棋盘坐标 -- j
+     * @param color 颜色
+     */
+    drawChess(i, j, color) {
+      let context = this.context
+      context.beginPath()
+      context.arc(m + i * d, m + j * d, r, 0, 2 * Math.PI)
+      context.closePath()
+      if (color === COLOR.BLACK.CODE) {
+        context.fillStyle = COLOR.BLACK.COLOR
+        context.fill()
+      } else if (color === COLOR.WHITE.CODE) {
+        context.stroke()
+        context.fillStyle = COLOR.WHITE.COLOR
+        context.fill()
+      }
+    },
+    /**
+     *
+     * 画出高亮棋子
+     * @param i 坐标
+     * @param j 坐标
+     */
+    drawLabel(i, j) {
+      let context = this.context
+      context.beginPath()
+      context.arc(m + i * d, m + j * d, r / 3, 0, 2 * Math.PI)
+      context.closePath()
+      context.fillStyle = COLOR.LABEL_COLOR
+      context.fill()
+    },
+    /**
+     *
+     * 判断格点是否已经被下过棋子
+     * @param i 坐标 i
+     * @param j 坐标 j
+     * @returns {boolean} 已经下过则返回true，没有则返回false
+     */
+    hasStep(i, j) {
+      for (let step of this.steps) {
+        if (step.i === i && step.j === j) {
+          return true
+        }
+      }
+      return false
+    },
+
   },
   mounted() {
     this.initCanvas();
