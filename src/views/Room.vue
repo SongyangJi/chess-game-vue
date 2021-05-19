22d544b09991514d7488cbbb2e1ab72cbea0f96c
SongyangJi <1539242799@qq.com>
2021-05-19 23:48:26 +0800

    游戏房间界面

---
+++ src/views/Room.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-row>
+    <el-col :span="5">
+      <Dialog dialog-title="聊天对话框"></Dialog>
+    </el-col>
+    <el-col :span="14">
+      <ChessBoard></ChessBoard>
+    </el-col>
+    <el-col :span="5">
+      <GameDetail></GameDetail>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import ChessBoard from "@/components/ChessBoard";
+import Dialog from "@/components/Dialog";
+import GameDetail from "@/components/GameDetail";
+
+export default {
+  name: "Room",
+  components: {ChessBoard, Dialog, GameDetail}
+}
+</script>
+
+<style scoped>
+
+</style>
