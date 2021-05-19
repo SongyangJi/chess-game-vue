6c260be2318bd5858146fe8d649b2d2d6aa5b950
SongyangJi <1539242799@qq.com>
2021-05-19 23:47:25 +0800

    大厅

---
+++ src/views/Hall.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-row>
+    <el-col :span="5">
+      <Dialog dialog-title="大厅闲聊"/>
+    </el-col>
+    <el-col :span="14">
+      <RoomsTable></RoomsTable>
+    </el-col>
+    <el-col :span="5">
+      <PlayersTable></PlayersTable>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+
+import RoomsTable from "@/components/RoomsTable"
+import Dialog from "@/components/Dialog";
+import PlayersTable from "@/components/PlayersTable";
+
+export default {
+  name: "Hall",
+  components: {
+    RoomsTable,
+    Dialog,
+    PlayersTable
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
