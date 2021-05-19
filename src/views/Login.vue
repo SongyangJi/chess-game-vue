a4db7f1410362cb0bcd7ac07586399fbd5615c6b
SongyangJi <1539242799@qq.com>
2021-05-19 23:47:53 +0800

    登录界面

---
+++ src/views/Login.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="shadow-container">
+    <h2 class="title">快来下五子棋吧！</h2>
+    <el-divider/>
+    <el-form class="form" @submit.native.prevent>
+      <el-form-item>
+        <el-input v-model="nickname" placeholder="请输入你的昵称" @keyup.enter.native="login"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="login">游客登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import {getRequest} from "@/utils/api"
+
+export default {
+  name: "LoginComponent",
+  data() {
+    return {
+      nickname: ''
+    }
+  },
+  methods: {
+    login() {
+      let api = '/api/visitor/' + this.nickname;
+      getRequest(api).then(res => {
+        console.log(res.data)
+        if (res.status === 200) {
+          alert("将进入大厅")
+          let id = {
+            uid: res.data,
+            nickname: this.nickname
+          }
+          this.$store.commit('setId', id)
+          this.$router.replace({path: '/layout/room'});
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.shadow-container {
+  margin: 180px auto;
+  width: 25%;
+  text-align: center;
+  min-width: 400px;
+}
+
+.form {
+  width: 60%;
+  padding: 3% 1% 1% 20%;
+}
+</style>
