f5987bfd321126ef15abc0ff5f41d99637450364
SongyangJi <1539242799@qq.com>
2021-05-22 01:16:45 +0800

    采用 JavaScript的面向对象手段封装 socket.js 工具类

--- src/websocket/socket.js
+++ src/websocket/socket.js
@@ -1,54 +1,60 @@
-import opt from "@/constants/chessOperations"
-import handler from "@/websocket/messageHandler"
+/**
+ *
+ * @constructor
+ */
 
-let ws = null
-
-export default {
-    connect(url) {
-        ws = new WebSocket(url)
+export function Socket() {
+    this.ws = null
+    this.connect = function (url) {
+        this.ws = new WebSocket(url)
+        let ws = this.ws
         ws.onopen = onOpen
         ws.onclose = onClose
         ws.onerror = onError
         ws.onmessage = onMessage
-    },
-    send(data) {
-        ws.send(data)
+
+        // 监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
+        window.onbeforeunload = function () {
+            ws.close();
+        }
+
+    }
+    this.send = function (data) {
+        this.ws.send(JSON.stringify(data))
+    }
+    this.setOnOpen = function (onopen) {
+        this.ws.onopen = onopen
+    }
+    this.setOnClose = function (onclose) {
+        this.ws.onclose = onclose
+    }
+    this.setOnError = function (onerror) {
+        this.ws.onerror = onerror
+    }
+    this.setMessageHandler = function (handler) {
+        this.ws.onmessage = handler
     }
 }
+
+/**
+ * 几个默认的监听回调方法
+ *
+ */
 
 function onOpen(e) {
     console.log('websocket 打开', e)
 }
-
 
 function onClose(e) {
     console.log('websocket 断开: ', '错误状态码', e.code, '断开原因', e.reason, '是否正常断开', e.wasClean,)
     console.log(e)
 }
 
-
 function onMessage(e) {
     console.log('收到消息 ', e)
-    let data = e.data
-    console.log('socket信息', data)
-    switch (data.opt) {
-        case opt.STEP:
-            handler.showStep(data)
-            break
-        case opt.OVER:
-            handler.gameOver(data)
-            break
-        default:
-            break
-    }
 }
 
 function onError(e) {
     console.log('websocket 异常', e)
 }
 
-
-// 监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
-window.onbeforeunload = function () {
-    ws.close();
-}
