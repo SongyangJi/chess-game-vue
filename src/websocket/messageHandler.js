9f5365eb8b3324ea372094bf11a1920bf282be02
SongyangJi <1539242799@qq.com>
2021-05-22 01:18:05 +0800

    集成在 特定的service.js里

--- src/websocket/messageHandler.js
+++
@@ -1,9 +0,0 @@
-export default {
-    showStep(data) {
-        this.$store.commit("step", data.dot)
-    },
-    gameOver(data) {
-        this.$store.commit("updateGameState",data.opt)
-        this.$store.commit("setWinner",data.role)
-    }
-}
