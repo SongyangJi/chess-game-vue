672845682c16120d948231c9b107dded24110e43
SongyangJi <1539242799@qq.com>
2021-05-28 02:36:51 +0800

    增加了游戏内对局信息 —— 显示自己的身份，准备游戏

--- src/components/GameDetail.vue
+++ src/components/GameDetail.vue
@@ -1,14 +1,79 @@
 <template>
   <div class="container">
-    <div class="header">
-      <span class="title">对局信息</span>
-    </div>
+
+    <el-container>
+      <el-header>
+        <div class="header">
+          <span class="title">对局信息</span>
+        </div>
+      </el-header>
+      <el-main>
+
+        <el-row>
+          <el-col :span="12" :offset="8">
+            <el-avatar :size="50" src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"></el-avatar>
+          </el-col>
+        </el-row>
+
+
+        <el-row>
+          <el-col :span="12">
+            <div>我的昵称</div>
+          </el-col>
+          <el-col :span="12">
+            <div>{{ selfRole.nickName }}</div>
+          </el-col>
+
+        </el-row>
+
+
+        <el-row>
+          <el-col :span="12">
+            <div v-if="selfRole.turn===0">房主</div>
+            <div v-else-if="selfRole.turn===1">挑战者</div>
+            <div v-else>观战者</div>
+          </el-col>
+          <el-col :span="12">
+            <div v-if="selfRole.turn===0">黑方</div>
+            <div v-else-if="selfRole.turn===1">白方</div>
+          </el-col>
+        </el-row>
+
+        <el-divider></el-divider>
+
+        <el-row>
+          <el-col :span="12">
+            点击准备
+          </el-col>
+          <el-col :span="12">
+            <el-button type="success" icon="el-icon-check" circle @click="setReady"></el-button>
+          </el-col>
+        </el-row>
+
+
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
+
+import {postRequest} from "@/utils/api";
+
 export default {
-name: "GameDetail"
+  name: "GameDetail",
+  props: ['roomId', 'selfRole'],
+  methods: {
+    setReady() {
+      let api = '/api/game/room/' + this.roomId
+      postRequest(api, this.selfRole).then(res=>{
+        if(res.status === 200) {
+          alert('已准备，等待游戏开始！')
+        }
+      }).catch(
+      )
+    }
+  }
 }
 </script>
 
@@ -16,25 +81,31 @@ name: "GameDetail"
 .container {
   margin-top: 3%;
 }
+
 .scrollbar {
   height: calc(60vh);
   min-height: 180px;
 }
+
 .header {
   border-bottom: 1px solid lightgrey;
   padding: 2% 0 2% 5%;
 }
+
 .title {
   padding-top: 100px;
 }
+
 .line {
   margin-top: 5px;
   margin-left: 3%;
   font-size: small;
 }
+
 .input {
   width: 80%;
 }
+
 .send-button {
   width: 20%;
 }
